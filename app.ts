3ba35b496a303f3111dcc55d0c1c5c368d61cfe0
Luis Faria <lfariabr@gmail.com>
2025-05-06 16:13:59 +0800

    (feat)types-and-data: added the Types, Input and sent request to validate implementation

--- app.ts
+++ app.ts
@@ -5,17 +5,36 @@ import { buildSchema } from 'graphql';
 
 const app = express();
 
+const events: any[] = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
-    type RootQuery {
-      events: [String!]! 
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
     }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    
+    type RootQuery {
+      events: [Event!]! 
+    }
+
     type RootMutation {
-      createEvent(name: String!): String!
+      createEvent(eventInput: EventInput): Event
       
     }
+
     schema {
       query: RootQuery
       mutation: RootMutation
@@ -23,11 +42,19 @@ app.use('/graphql', graphqlHTTP({
   `),
   rootValue: {
     events: () => {
-      return ['Romantic Cooking', 'Running', 'Swimming']
+      return events;
     },
     createEvent: (args: any) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title, // nested object, so we need to access it with .eventInput
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: args.eventInput.date
+      }
+      console.log(args); // Nested object return
+      events.push(event);
+      return event;
     }
   },
   graphiql: true,
