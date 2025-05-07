3961376d83a4f2ba0aba66dbe02e69b946c1e6f9
Luis Faria <lfariabr@gmail.com>
2025-05-07 10:58:21 +0800

    (feat)mongo-db: plugged in mongoDB, tested connection, created mutations and adjusted queries to return data both async and sync

--- app.ts
+++ app.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
+import mongoose from 'mongoose';
+import Event from './models/Event';
 
 const app = express();
-
-const events: any[] = [];
 
 app.use(bodyParser.json());
 
@@ -42,27 +42,52 @@ app.use('/graphql', graphqlHTTP({
   `),
   rootValue: {
     events: () => {
-      return events;
+      return Event.find().then(events => {
+        return events.map(event => {
+          const formattedDate = typeof event.date === 'number'
+          ? new Date(event.date).toISOString()
+          : event.date instanceof Date
+        ? event.date.toISOString()
+        : event.date;
+        return {
+          _id: event._id.toString(),
+          title: event.title,
+          description: event.description,
+          price: event.price,
+          date: formattedDate
+        };
+      });
+    });
     },
     createEvent: (args: any) => {
-      const event = {
-        _id: Math.random().toString(),
-        title: args.eventInput.title, // nested object, so we need to access it with .eventInput
+      const event = new Event({
+        title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: args.eventInput.date
-      }
-      console.log(args); // Nested object return
-      events.push(event);
-      return event;
+        date: new Date(args.eventInput.date)
+      })
+      return event
+        .save()
+        .then(result => {
+          console.log(result);
+          return result;
+        })
+        .catch(error => {
+          console.log(error);
+          throw error;
+        });
     }
   },
   graphiql: true,
 }))
 
-// app.get('/', (req, res, next) => {
-//   res.send('Hello World!');
-// });
+mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB}.qjxuqu5.mongodb.net/?retryWrites=true&w=majority&appName=EventReactGraphQL`)
+  .then(() => {
+    console.log('Connected to MongoDB! 🎉');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
